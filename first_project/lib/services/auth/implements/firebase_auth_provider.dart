92eae6663e28b5e55232497196efcbc56613a28b
michelpaliz <michelpaliz@hotmail.com>
2023-06-14 23:01:58 +0200

    fixing the firebase init

--- first_project/lib/services/auth/implements/firebase_auth_provider.dart
+++ first_project/lib/services/auth/implements/firebase_auth_provider.dart
@@ -96,7 +96,7 @@ class FirebaseAuthProvider implements AuthProvider {
 
   @override
   Future<void> initialize() async {
-    Firebase.initializeApp(
+    await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
   }
