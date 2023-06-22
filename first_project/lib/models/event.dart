cec6fbbc7280a11667a39dec9d0d47c0f82a9a96
michelpaliz <michelpaliz@hotmail.com>
2023-06-22 17:30:56 +0200

    putting a calendar

---
+++ first_project/lib/models/event.dart
@@ -0,0 +1,9 @@
+class Event {
+  final DateTime date;
+  // Add other properties as needed
+
+  Event({
+    required this.date,
+    // Initialize other properties as needed
+  });
+}
--- first_project/lib/view/notes_view.dart
+++ first_project/lib/view/notes_view.dart
@@ -1,6 +1,4 @@
 import 'dart:developer' as devtools show log;
-import 'dart:html';
-
 import 'package:first_project/services/auth/implements/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -8,14 +6,29 @@ import 'package:table_calendar/table_calendar.dart';
 import '../constants/routes.dart';
 import '../enums/menu_action.dart';
 
+class Event {
+  final DateTime date;
+  // Add other properties as needed
+
+  Event({
+    required this.date,
+    // Initialize other properties as needed
+  });
+}
+
 class NotesView extends StatefulWidget {
-  const NotesView({super.key});
+  const NotesView({Key? key}) : super(key: key);
 
   @override
   State<NotesView> createState() => NotesViewState();
 }
 
 class NotesViewState extends State<NotesView> {
+  List<Event> eventsList = [
+    Event(date: DateTime(2023, 6, 1)),
+    Event(date: DateTime(2023, 6, 15)),
+    // Add more events with different dates
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,7 +67,10 @@ class NotesViewState extends State<NotesView> {
             // Return a list of events/notes for the date
             return getEventsForDate(
                 date); // Replace this with your implementation
-          }, firstDay: null, focusedDay: null, lastDay: null,
+          },
+          firstDay: DateTime.utc(2023, 1, 1),
+          focusedDay: DateTime.now(),
+          lastDay: DateTime.utc(2023, 12, 31),
           // Add other customization options as needed
           // ...
         ),
@@ -67,39 +83,39 @@ class NotesViewState extends State<NotesView> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text('Sing out'),
-          content: const Text('Are you sure you want to sign out ?'),
+          title: const Text('Sign out'),
+          content: const Text('Are you sure you want to sign out?'),
           actions: [
             TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(false);
-                },
-                child: const Text('Cancel')),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel'),
+            ),
             TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: const Text('Log out'))
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out'),
+            )
           ],
         );
       },
     ).then((value) => value ?? false);
   }
-}
 
-List<Event> getEventsForDate(DateTime date) {
-  // Implement your logic to retrieve events or notes for the specified date
-  // Return a list of events/notes for the date
+  List<Event> getEventsForDate(DateTime date) {
+    // Implement your logic to retrieve events or notes for the specified date
+    // Return a list of events/notes for the date
 
-  // Example implementation:
-  // Assuming you have a list of events called 'eventsList'
-  // Filter the events based on the date and return the matching events
-  var eventsList = null;
-  final eventsForDate = eventsList.where((event) {
-    return event.date.year == date.year &&
-        event.date.month == date.month &&
-        event.date.day == date.day;
-  }).toList();
+    // Example implementation:
+    // Filter the events based on the date and return the matching events
+    final eventsForDate = eventsList.where((event) {
+      return event.date.year == date.year &&
+          event.date.month == date.month &&
+          event.date.day == date.day;
+    }).toList();
 
-  return eventsForDate;
+    return eventsForDate;
+  }
 }
